3ca236539cc6e5d468488e7682e33be096e4707f
fred_la_bete@hotmail.com <fred_la_bete@hotmail.com>
2025-04-02 12:19:10 +0200

    push 12h15 02/04/25

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
-const path = require("path");
 const app = express();
+const path = require("path");
+require("dotenv").config();
 
 app.set("view engine", "ejs");
 
