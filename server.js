325ac213fd9697badd1f1e4f66e78ba5d34152bd
fred_la_bete@hotmail.com <fred_la_bete@hotmail.com>
2025-04-02 16:11:26 +0200

    16h15 02/04/25

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 require("dotenv").config();
+const { fetchData } = require("./Public/assets/js/lib/functions");
 
 app.set("view engine", "ejs");
 
@@ -10,9 +11,22 @@ app.use(express.static(path.join(__dirname, "public"))); // => /front/public/
 app.get("/", (req, res) => {
   res.render("index");
 });
+
 app.get("/contact", (req, res) => {
   res.render("pages/contact");
 });
+
+app.get("/masonry", (req, res) => {
+  res.render("pages/masonry", {images: imagesTab});
+});
+
+const images = fetchData({
+  route:
+    "/games?key=de462d1e145d44e084148f017bf5976d&dates=2019-09-01,2019-09-30&platforms=18,1,7",
+}).then((data) => {
+  return data.results;
+});
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
